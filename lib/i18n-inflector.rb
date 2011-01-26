5d81d7514ad94a12b72e7ffcb2f2168511a52f2f
Paweł Wilk <siefca@gnu.org>
2011-01-26 01:04:27 +0100

    Added new error classes InflectionOptionNotFound and InflectionOptionIncorrect

--- lib/i18n-inflector.rb
+++ lib/i18n-inflector.rb
@@ -3,14 +3,14 @@
 require 'i18n'
 
 require 'i18n-inflector/version'
-require 'i18n-inflector/errors'
 require 'i18n-inflector/lazy_enum'
 require 'i18n-inflector/inflection_data_strict'
 require 'i18n-inflector/inflection_data'
 require 'i18n-inflector/options'
 require 'i18n-inflector/backend'
+require 'i18n-inflector/inflector'
+require 'i18n-inflector/errors'
 require 'i18n-inflector/api_strict'
 require 'i18n-inflector/api'
-require 'i18n-inflector/inflector'
 
 I18n::Backend::Simple.send(:include, I18n::Backend::Inflector)
--- lib/i18n-inflector/errors.rb
+++ lib/i18n-inflector/errors.rb
@@ -8,64 +8,102 @@
 
 module I18n
 
-  # This is raised when there is no kind given in options or the kind is +nil+. The kind
-  # is determined by looking at token placed in a pattern.
-  # 
-  # This exception will also be raised when a required option, describing token selected
-  # for a kind, is empty or doesn't match any acceptable tokens.
+  # @abstract This class is a parent class for exceptions raised when
+  #   inflection option is bad or missing.
   class InvalidOptionForKind < ArgumentError
+
     attr_reader :pattern, :kind, :token, :option
+
     def initialize(pattern, kind, token, option)
-      @pattern, @kind, @token, @option = pattern, kind, token, option
-      if option.nil?
-        super "option #{kind.inspect} required by the " +
-              "pattern #{pattern.inspect} was not found"
-      else
-        super "value #{option.inspect} of #{kind.inspect} required by the " +
-              "pattern #{pattern.inspect} does not match any token"      
-      end
+      @pattern, @kind, @token, @option, @option_present = pattern, kind, token, option
+      @message ||= ""
+      super(@message)
     end
+
+  end
+
+  # This is raised when there is no kind given in options. The kind
+  # is determined by looking at token placed in a pattern.
+  class InflectionOptionNotFound < InvalidOptionForKind
+
+    def initialize(pattern, kind, token, option=nil)
+      kind = kind.to_s
+      unless kind.empty?
+        if kind[0..0] == I18n::Inflector::NAMED_MARKER
+          kind = ":#{kind} (or :#{kind[1..-1]})"
+        else
+          kind = kind.to_sym.inspect
+        end
+      end
+      @message = "option #{kind} required by the " +
+                 "pattern #{pattern.inspect} was not found"  
+      super
+    end
+
+  end
+
+  # This exception will be raised when a required option, describing token selected
+  # for a kind, is +nil+, empty or doesn't match any acceptable tokens.
+  class InflectionOptionIncorrect < InvalidOptionForKind
+
+    def initialize(pattern, kind, token, option)
+      @message = "value #{option.inspect} of option #{kind.inspect} required by " +
+                 "#{pattern.inspect} does not match any token"
+      super
+    end
+
   end
 
   # This is raised when token given in pattern is invalid (empty or has no
   # kind assigned).
   class InvalidInflectionToken < ArgumentError
+
     attr_reader :pattern, :token
+
     def initialize(pattern, token)
       @pattern, @token = pattern, token
       super "token #{token.inspect} used in translation " + 
             "pattern #{pattern.inspect} is invalid"
     end
+
   end
 
   # This is raised when an inflection token used in a pattern does not match
   # an assumed kind determined by reading previous tokens from that pattern.
   class MisplacedInflectionToken < ArgumentError
+
     attr_reader :pattern, :token, :kind
+
     def initialize(pattern, token, kind)
       @pattern, @token, @kind = pattern, token, kind
       super "inflection token #{token.inspect} from pattern #{pattern.inspect} " +
             "is not of the expected kind #{kind.inspect}"
     end
+
   end
 
   # This is raised when an inflection token of the same name is already defined in
   # inflections tree of translation data.
   class DuplicatedInflectionToken < ArgumentError
+
     attr_reader :original_kind, :kind, :token
+
     def initialize(original_kind, kind, token)
       @original_kind, @kind, @token = original_kind, kind, token
       and_cannot = kind.nil? ? "" : "and cannot be used with kind #{kind.inspect}"
       super "inflection token #{token.inspect} was already assigned " +
             "to kind #{original_kind} " + and_cannot
     end
+
   end
 
   # This is raised when an alias for an inflection token points to a token that
   # doesn't exists. It is also raised when default token of some kind points
   # to a non-existent token.
   class BadInflectionAlias < ArgumentError
+
     attr_reader :locale, :token, :kind, :pointer
+
     def initialize(locale, token, kind, pointer)
       @locale, @token, @kind, @pointer = locale, token, kind, pointer
       what = token == :default ? "default token" : "alias"
@@ -74,12 +112,15 @@ module I18n
       super "the #{what} #{token.inspect} " + kinn + lang +
             "points to an unknown token #{pointer.inspect}"
     end
+
   end
 
   # This is raised when an inflection token or its description has a bad name. This
   # includes an empty name or a name containing prohibited characters.
   class BadInflectionToken < ArgumentError
+
     attr_reader :locale, :token, :kind, :description
+
     def initialize(locale, token, kind=nil, description=nil)
       @locale, @token, @kind, @description = locale, token, kind, description
       kinn = kind.nil? ? "" : "of kind #{kind.inspect} "
@@ -91,6 +132,7 @@ module I18n
               "for language #{locale.inspect} has a bad description #{description.inspect}"
       end
     end
+
   end
 
 end
