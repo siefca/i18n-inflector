ed4cea2f3b809028b02fa19c089fecc7cf56713b
Paweł Wilk <siefca@gnu.org>
2011-02-15 02:39:26 +0100

    Release 2.3.1

--- lib/i18n-inflector/version.rb
+++ lib/i18n-inflector/version.rb
@@ -14,7 +14,7 @@ module I18n
     # @private
     EMAIL       = 'pw@gnu.org'
     # @private
-    VERSION     = '2.3.0'
+    VERSION     = '2.3.1'
     # @private
     NAME        = 'i18n-inflector'
     # @private
