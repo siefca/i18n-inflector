c274ae5e650ffc0abe8095c70104ae74e88c58ff
Paweł Wilk <siefca@gnu.org>
2011-02-15 02:36:58 +0100

    Fixed documentation links

--- lib/i18n-inflector/inflection_data_strict.rb
+++ lib/i18n-inflector/inflection_data_strict.rb
@@ -2,7 +2,7 @@
 #
 # Author::    Paweł Wilk (mailto:pw@gnu.org)
 # Copyright:: (c) 2011 by Paweł Wilk
-# License::   This program is licensed under the terms of {file:LGPL GNU Lesser General Public License} or {file:COPYING Ruby License}.
+# License::   This program is licensed under the terms of {file:docs/LGPL GNU Lesser General Public License} or {file:COPYING Ruby License}.
 # 
 # This file contains class that is used to keep
 # inflection data for strict kinds.
--- lib/i18n-inflector/interpolate.rb
+++ lib/i18n-inflector/interpolate.rb
@@ -2,7 +2,7 @@
 #
 # Author::    Paweł Wilk (mailto:pw@gnu.org)
 # Copyright:: (c) 2011 by Paweł Wilk
-# License::   This program is licensed under the terms of {file:LGPL GNU Lesser General Public License} or {file:COPYING Ruby License}.
+# License::   This program is licensed under the terms of {file:docs/LGPL GNU Lesser General Public License} or {file:COPYING Ruby License}.
 # 
 # This file contains I18n::Inflector::Interpolate module,
 # which is included in the API.
